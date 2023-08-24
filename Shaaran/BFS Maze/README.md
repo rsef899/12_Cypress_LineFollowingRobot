e03fd4e1e8ad3065245e6eee5c8b7dd0ee06d1b3
sela847 <sela847@aucklanduni.ac.nz>
2023-08-24 22:00:56 +1200

    Created the BFS algorithm
    This one is solid

---
+++ Shaaran/BFS Maze/README.md
@@ -0,0 +1,69 @@
+# Maze Visualiser
+
+This project has been created for the COMPSYS301 design
+project. It is meant to be able to visualise path finding
+code. I created it so that pathfinding code could be written
+in C and tested in C instead of porting C code over to
+MATLAB.
+
+To use this code, simply modify the algorithm inside
+`algo.c`. Currently, the algorithm uses a modified version
+of DFS, and is nowhere near optimal, so it shouldn't be
+used.
+
+## Requirements
+
+**Windows**:
+- `cmake`: A cross-platform build system ([link](cmake.org))
+- A C compiler
+
+*OR*
+
+- `Visual Studio`: Microsofts IDE for C/C++ and other
+  projects ([link](https://visualstudio.microsoft.com/#vs-section))
+
+**macOS / Linux**
+- `raylib`: C library for drawing visuals
+  ([link](https://www.raylib.com/))
+- A C compiler
+- `pkg-config`: Finds the correct verisons of installed
+  libraries ([link](https://www.freedesktop.org/wiki/Software/pkg-config/))
+
+If you're using macOS, you should probably use `brew` or
+`macports` to install the dependencies. i.e.
+```
+brew install pkg-config raylib
+```
+
+If you're using linux, you can just use your distros package
+manager to install the dependencies.
+
+If you're using Windows, you can install CMake from
+`cmake.org`
+***MAKE SURE TO ADD CMAKE TO PATH IN THE INSTALLER***.
+
+## Building
+
+If you're on macOS or Linux, you can simply just install the
+dependencies listed above, and then run `./build.sh`.
+
+### Building on Windows (Visual Studio)
+
+You can open CMake projects in Visual Studio. I'm not sure
+which button to click to do it, but I think it is simple
+enough.
+
+### Building on Windows (Command Line)
+
+On Windows, to setup cmake, you need to run
+```pwsh
+mkdir build # Create a build directory
+cd bin && cmake .. # Build from that directory so the build files are in one place
+```
+
+Once CMake is setup, to rebuild you code, you can just run
+```
+cmake --build . # Build the project
+```
+from inside the `bin` directory. You do not need to run
+the CMake setup more than once.
--- Shaaran/BFS Maze/algo.c
+++ Shaaran/BFS Maze/algo.c
@@ -1,15 +1,15 @@
+
+// finds shortes path from start to first endpoint
 #include "algo.h"
 #include "defs.h"
 
 #include <assert.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
 
 // Macros that allow easy creation of adjacent points
 // i.e. LEFT(start), will get you the position that is
 // left from the starting point.
-
 #define LEFT(p) ((Point){.x = (p).x - 1, .y = (p).y})
 #define RIGHT(p) ((Point){.x = (p).x + 1, .y = (p).y})
 #define ABOVE(p) ((Point){.x = (p).x, .y = (p).y - 1})
@@ -23,17 +23,45 @@
 // ends:
 //   The end points which the algorithm should search for
 // endCount:
-//	 The number of possible end points we have (size of array ends points to)
+//	 The number of possible end points we have (size of array ends points
+// to)
 // out:
 //   Where your final path should be written. It is an array.
 //
 // RETURNS:
 //  The length of the path
-AlgoResult run_algo(const uint8_t map[MAP_HEIGHT][MAP_WIDTH],
-                    Point start,
-                    const Point *ends,
-                    size_t endCount,
-                    Point *out) {
+bool isValid(Point p, const uint8_t map[MAP_HEIGHT][MAP_WIDTH]) {
+  return p.x >= 0 && p.x < MAP_WIDTH && p.y >= 0 && p.y < MAP_HEIGHT &&
+         map[p.y][p.x] == 0;
+}
+Point queue[MAP_HEIGHT * MAP_WIDTH];
+size_t front = 0;
+size_t rear = 0;
+size_t size = 0;
+
+bool isQueueEmpty() { return size == 0; }
+
+bool isQueueFull() { return size == MAP_HEIGHT * MAP_WIDTH; }
+
+void enqueue(Point element) {
+  if (!isQueueFull()) {
+    queue[rear] = element;
+    rear = (rear + 1) % (MAP_HEIGHT * MAP_WIDTH);
+    size++;
+  }
+}
+
+Point dequeue() {
+  Point element = {0}; // Default initialization
+  if (!isQueueEmpty()) {
+    element = queue[front];
+    front = (front + 1) % (MAP_HEIGHT * MAP_WIDTH);
+    size--;
+  }
+  return element;
+}
+size_t bfs(const uint8_t map[MAP_HEIGHT][MAP_WIDTH], Point start,
+           const Point *ends, size_t endCount, Point *out) {
   Point visited[MAP_HEIGHT][MAP_WIDTH] = {{0}};
   uint8_t dummyMap[MAP_HEIGHT][MAP_WIDTH];
   for (uint8_t i = 0; i < MAP_HEIGHT; i++) {
@@ -41,31 +69,30 @@ AlgoResult run_algo(const uint8_t map[MAP_HEIGHT][MAP_WIDTH],
       dummyMap[i][j] = map[i][j];
     }
   }
+  uint8_t found = 0;
+  size_t pathIndex = 0;
+  uint8_t foundEndsCount = 0;
   Point foundEnds[endCount];
-  size_t foundEndsCount = 0;
-  uint16_t outIndex = 0;
-  
-  Point stack[MAP_HEIGHT * MAP_WIDTH];
-  size_t stackTop = 0;
-  stack[stackTop++] = start;
   Point newStart = start;
+  enqueue(start);
 
-  while (stackTop > 0) {
-    uint8_t found = 0;
-    Point curr = stack[--stackTop];
+  while (!isQueueEmpty()) {
+    found = 0;
+    Point curr = dequeue();
     Point left = LEFT(curr);
     Point right = RIGHT(curr);
     Point above = ABOVE(curr);
     Point below = BELOW(curr);
-    for (uint8_t j = 0; j < endCount; j++) {
-      if (point_eq(curr, ends[j])) {
-        for (uint16_t i = 0; i < endCount; i++) {
-          if (point_eq(curr, foundEnds[i])) {
+    for (size_t i = 0; i < endCount; i++) {
+      if (point_eq(curr, ends[i])) {
+        for (uint8_t j = 0; j < endCount; j++) {
+          if (point_eq(curr, foundEnds[j])) {
             found = 1;
           }
         }
         if (found == 0) {
-          Point shortestPath[MAP_HEIGHT * MAP_WIDTH];
+          // Reconstruct the path to the endpoint
+          Point shortestPath[MAP_HEIGHT*MAP_WIDTH];
           size_t shortestPathIndex = 0;
           Point traceBack = curr;
 
@@ -78,7 +105,7 @@ AlgoResult run_algo(const uint8_t map[MAP_HEIGHT][MAP_WIDTH],
 
           // Copy the reconstructed path to the 'out' array
           for (int i = shortestPathIndex - 1; i >= 0; i--) {
-            out[outIndex++] = shortestPath[i];
+            out[pathIndex++] = shortestPath[i];
           }
 
           // Reset visited array for the next search
@@ -88,42 +115,49 @@ AlgoResult run_algo(const uint8_t map[MAP_HEIGHT][MAP_WIDTH],
               dummyMap[i][j] = map[i][j];
             }
           }
+          front = 0;
+          rear = 0;
+          size = 0;
 
           newStart = curr;
-          // Reset stack for the next search
-          stackTop = 0;
-
           foundEnds[foundEndsCount] = curr;
           // Increment the found ends count:
           foundEndsCount++;
 
           if (foundEndsCount == endCount) {
             // Return the shortest path length
-            return (AlgoResult){.pathLength = outIndex};
+            return pathIndex;
           }
         }
       }
     }
-    // Mark the current node as visited (non-zero)
     dummyMap[curr.y][curr.x] = 1;
-    if (dummyMap[left.y][left.x] == 0) {
-      stack[stackTop++] = left;
+    if (isValid(left, dummyMap)) {
+      enqueue(left);
       visited[left.y][left.x] = curr; // Store the parent
     }
-    if (dummyMap[right.y][right.x] == 0) {
-      stack[stackTop++] = right;
+    if (isValid(right, dummyMap)) {
+      enqueue(right);
       visited[right.y][right.x] = curr; // Store the parent
     }
-    if (dummyMap[above.y][above.x] == 0) {
-      stack[stackTop++] = above;
+    if (isValid(above, dummyMap)) {
+      enqueue(above);
       visited[above.y][above.x] = curr; // Store the parent
     }
-    if (dummyMap[below.y][below.x] == 0) {
-      stack[stackTop++] = below;
+    if (isValid(below, dummyMap)) {
+      enqueue(below);
       visited[below.y][below.x] = curr; // Store the parent
     }
+
+    
   }
-  return (AlgoResult){.pathLength = 0};
+  return 0;
+}
+
+AlgoResult run_algo(const uint8_t map[MAP_HEIGHT][MAP_WIDTH], Point start,
+                    const Point *ends, size_t endCount, Point *out) {
+  size_t pathLength = bfs(map, start, ends, endCount, out);
+  return (AlgoResult){.pathLength = pathLength};
 }
 
 // Don't need to modify this
